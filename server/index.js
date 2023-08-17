b4f2cb1cdda1fd497bff598511be032c41605144
Adnan1O <adnn4u@gmail.com>
2023-08-17 17:00:07 +0530

    day 3 second commit

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
 const cors = require("cors")
 const port = 5000
 app.use(cors)
 app.use(express.json())
+
+mongoose.connect(
+    "mongodb+srv://adnn4u:adnan1882@cluster0.axlobi4.mongodb.net/myntra?retryWrites=true&w=majority"
+)
+
+
 
 app.listen(port, ()=>{console.log(`myntra is flaying at ${port}`)})
