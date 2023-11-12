a5bd729113d044352aa19e167be89b3fa3121f5e
Adnan1O <adnn4u@gmail.com>
2023-11-12 15:34:14 +0530

    text formatted

--- src/components/Singleproduct/Singleproduct.js
+++ src/components/Singleproduct/Singleproduct.js
@@ -11,7 +11,6 @@ function Singleproduct() {
   const { productId } = useParams();
   const [productData, setProductData] = useState({});
   const singleData= async()=>{
-
     try {
       const response = await fetch(`http://localhost:5000/singleproduct/${productId}`)
       const jsonData = await response.json()
