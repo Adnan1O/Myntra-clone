2279bf66d8850709d31f69f0c779d7520b7749d0
adnan shaikh <adnn4u@gmail.com>
2024-02-16 20:19:17 +0530

    pro revealed

--- src/components/Singleproduct/ProCat.js
+++ src/components/Singleproduct/ProCat.js
@@ -1,20 +1,20 @@
-// import React from 'react'
-// import "./ProCat.css"
+import React from 'react'
+import "./ProCat.css"
 
-// function ProCat() {
-//   return (
-//     <div className='ProCat'>
-//         <div className="cat-area">
-//       <p>Home</p><p>/</p>
-//       <p>Clothing</p><p>/</p>
-//       <p>Women Clothing</p><p>/</p>
-//       <p>Tshirt</p><p>/</p>
-//       <span>brand Name</span> 
-//       <span>&gt;</span>
-//       <span>More By brand Name</span>
-//     </div>
-//     </div>
-//   )
-// }
+function ProCat() {
+  return (
+    <div className='ProCat'>
+        <div className="cat-area">
+      <p>Home</p><p>/</p>
+      <p>Clothing</p><p>/</p>
+      <p>Women Clothing</p><p>/</p>
+      <p>Tshirt</p><p>/</p>
+      <span>brand Name</span> 
+      <span>&gt;</span>
+      <span>More By brand Name</span>
+    </div>
+    </div>
+  )
+}
 
-// export default ProCat
+export default ProCat
